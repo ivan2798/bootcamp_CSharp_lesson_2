1fe5e4e61773d5dd875c06e54a6bacf1793ebcf6
ivan2798 <inge1998@hotmail.com>
2022-10-17 12:30:07 -0500

    Métodos I

---
+++ C_Sharp_lesson2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// Hello World! program
+namespace C_Sharp_lesson2
+{
+    class Program
+    {
+        // intellisense
+
+        static void Main(string[] args)
+        {
+            sumaNumeros(7,9);
+        }
+
+        static void sumaNumeros(int num1, int num2)
+        {
+            Console.WriteLine($"La suma de los numeros es: {num1+num2}");
+        }
+
+
+
+
+    }
+}
